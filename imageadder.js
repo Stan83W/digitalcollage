a230f54ec2326dc78790089324c718d544226a23
stan wt <stanislas.wiart@gmail.com>
2018-09-14 15:48:24 +0200

    halfway

---
+++ imageadder.js
@@ -0,0 +1,15 @@
+
+const images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg", "img7.jpg", "img8.jpg"]
+
+
+let i = 0
+
+function placeImage(x, y) {
+
+  const nextImage = images[i]
+
+}
+
+placeImage(500, 400)
+placeImage(400, 500)
+placeImage(600, 300)
